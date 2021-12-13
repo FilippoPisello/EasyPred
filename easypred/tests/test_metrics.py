b04ca3905a265e842d9ba86827ca7606761dba16
Filippo Pisello <filippo.pisello@live.it>
2021-12-13 18:52:36 +0100

    feat: remaining metrics added All the desidred metrics + docs have been added to the file

--- easypred/tests/test_metrics.py
+++ easypred/tests/test_metrics.py
@@ -1,21 +1,57 @@
-from unittest import TestCase
-
 import easypred.metrics as met
 import numpy as np
 import pandas as pd
+import pytest
+
+df = pd.read_excel("easypred/tests/test_data/binary.xlsx")
+real, fitted = df["Real"], df["Fitted"]
+val_positive = 1
 
 
-class TestMetrics(TestCase):
-    df = pd.read_excel("easypred/tests/test_data/binary.xlsx")
-    real_values, fitted_values = df["Real"], df["Fitted"]
-    value_positive = 1
+@pytest.mark.parametrize(
+    "real_values, fitted_values, accuracy",
+    [
+        (real, fitted, 439 / 500),
+        (np.array([1, 1, 0]), np.array([1, 1, 0]), 1),
+        (np.array([1, 1, 1]), np.array([1, 1, 0]), 2 / 3),
+    ],
+)
+def test_accuracy(real_values, fitted_values, accuracy):
+    assert met.accuracy_score(real_values, fitted_values) == accuracy
 
-    def test_accuracy(self):
-        accuracy = met.accuracy_score(self.real_values, self.fitted_values)
-        self.assertEqual(accuracy, 439 / 500)
 
-        accuracy = met.accuracy_score(np.array([1, 1, 0]), np.array([1, 1, 0]))
-        self.assertEqual(accuracy, 1)
+def test_balanced_accuracy():
+    assert met.balanced_accuracy_score(real, fitted, val_positive) == 0.8599422894294689
 
-        accuracy = met.accuracy_score(np.array([1, 1, 1]), np.array([1, 1, 0]))
-        self.assertEqual(accuracy, 2 / 3)
+
+def test_fp_rate():
+    score = met.false_positive_rate(real, fitted, val_positive)
+    assert score == (30 / (30 + 308))
+
+
+def test_fn_rate():
+    score = met.false_negative_rate(real, fitted, val_positive)
+    assert score == (31 / (31 + 131))
+
+
+def test_precision():
+    score = met.precision_score(real, fitted, val_positive)
+    assert score == (131 / (30 + 131))
+
+
+def test_negative_pred_value():
+    score = met.negative_predictive_value(real, fitted, val_positive)
+    assert score == (308 / (308 + 31))
+
+
+def test_recall():
+    score = met.recall_score(real, fitted, val_positive)
+    assert score == (131 / (31 + 131))
+
+
+def test_specificity():
+    score = met.specificity_score(real, fitted, val_positive)
+    assert score == (308 / (30 + 308))
+
+def test_f1rate():
+    assert met.f1_score(real, fitted, val_positive) == 0.8111455108359134
