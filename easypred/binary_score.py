29ae7a47cca17c75eefe8eb9716e0a6431e7d9b3
Filippo <filippo.pisello@live.it>
2021-12-26 19:32:20 +0100

    feat: functions for basic score handling Unique scores and values conversion to handle more complex operations

--- easypred/binary_score.py
+++ easypred/binary_score.py
@@ -22,3 +22,47 @@ class BinaryScore:
     def value_negative(self) -> Any:
         """Return the value that it is not the positive value."""
         return other_value(self.real_values, self.value_positive)
+
+    def unique_scores(self, decimals: int = 3) -> VectorPdNp:
+        """Return the unique values attained by the fitted scores, sorted in
+        ascending order.
+
+        Parameters
+        ----------
+        decimals : int, optional
+            The number of decimals the fitted scores should be rounded to before
+            deriving the unique values. It helps speeding up the operations in
+            the case of large datasets. By default 3
+
+        Returns
+        -------
+        np.ndarray | pd.Series
+            The array containing the sorted unique values. Its type matches
+            fitted_scores' type.
+        """
+        if isinstance(self.fitted_scores, np.ndarray):
+            return np.unique(self.fitted_scores.round(decimals))
+        return self.fitted_scores.round(decimals).unique().sort_values(ascending=True)
+
+    def score_to_values(self, threshold: float = 0.5) -> VectorPdNp:
+        """Return an array contained fitted values derived on the basis of the
+        provided threshold.
+
+        Parameters
+        ----------
+        threshold : float, optional
+            The minimum value such that the score is translated into
+            value_positive. Any score below the threshold is instead associated
+            with the other value. By default 0.5.
+
+        Returns
+        -------
+        np.ndarray | pd.Series
+            The array containing the inferred fitted values. Its type matches
+            fitted_scores' type.
+        """
+        return np.where(
+            (self.fitted_scores >= threshold),
+            self.value_positive,
+            self.value_negative,
+        )
