5d268fb50fb103ac75127f8f93df4a67cd7267d2
Filippo <filippo.pisello@live.it>
2021-12-26 17:55:45 +0100

    fix: sort imports to avoid circular dependencies

--- easypred/__init__.py
+++ easypred/__init__.py
@@ -1,4 +1,6 @@
+# Import first to avoid circular dependencies from binary_pred
+from easypred.binary_score import BinaryScore
+
 from easypred.base_prediction import Prediction
 from easypred.binary_prediction import BinaryPrediction
-from easypred.binary_score import BinaryScore
 from easypred.numeric_prediction import NumericPrediction
