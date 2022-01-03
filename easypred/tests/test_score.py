3270e8296697baaf1e71a7862644d48587f3274a
Filippo <filippo.pisello@live.it>
2022-01-03 12:42:49 +0100

    tests: naming changes

--- easypred/tests/test_score.py
+++ easypred/tests/test_score.py
@@ -13,7 +13,7 @@ def test_value_negative():
 
 
 @pytest.mark.parametrize(
-    "score, decimals, excpected",
+    "score, decimals, expected",
     [
         (score1, 2, np.array([0.25, 0.34, 0.66, 0.79])),
         (score1, 3, np.array([0.25, 0.341, 0.66, 0.79])),
@@ -21,15 +21,15 @@ def test_value_negative():
         (score2, 2, pd.Series([0.25, 0.34, 0.66, 0.79])),
     ],
 )
-def test_unique_score(score, decimals, excpected):
+def test_unique_score(score, decimals, expected):
     score.computation_decimals = decimals
-    np.testing.assert_array_equal(score.unique_scores, excpected)
+    np.testing.assert_array_equal(score.unique_scores, expected)
     # Restore default decimals
     score.computation_decimals = 3
 
 
 @pytest.mark.parametrize(
-    "score, threshold, excpected",
+    "score, threshold, expected",
     [
         (score1, 0.5, np.array([1, 0, 0, 1, 0])),
         (score1, 0, np.array([1, 1, 1, 1, 1])),
@@ -37,35 +37,35 @@ def test_unique_score(score, decimals, excpected):
         (score1, 0.7, np.array([1, 0, 0, 0, 0])),
     ],
 )
-def test_score_to_values(score, threshold, excpected):
+def test_score_to_values(score, threshold, expected):
     result = score.score_to_values(threshold=threshold)
-    np.testing.assert_array_equal(result, excpected)
+    np.testing.assert_array_equal(result, expected)
 
 
 @pytest.mark.parametrize(
-    "score, decimals, excpected",
+    "score, decimals, expected",
     [
         (score1, 3, np.array([2 / 5, 3 / 5, 1, 4 / 5])),
     ],
 )
-def test_accuracy_scores(score, decimals, excpected):
+def test_accuracy_scores(score, decimals, expected):
     score.computation_decimals = decimals
-    np.testing.assert_array_equal(score.accuracy_scores, excpected)
+    np.testing.assert_array_equal(score.accuracy_scores, expected)
 
 
 @pytest.mark.parametrize(
-    "score, decimals, excpected",
+    "score, decimals, expected",
     [
         (score1, 3, np.array([1, 2 / 3, 0, 0])),
     ],
 )
-def test_false_positive_rates(score, decimals, excpected):
+def test_false_positive_rates(score, decimals, expected):
     score.computation_decimals = decimals
-    np.testing.assert_array_equal(score.false_positive_rates, excpected)
+    np.testing.assert_array_equal(score.false_positive_rates, expected)
 
 
 @pytest.mark.parametrize(
-    "score, decimals, excpected",
+    "score, decimals, expected",
     [
         (score1, 3, np.array([1, 1, 1, 1 / 2])),
     ],
