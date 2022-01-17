10332e51c5e50aa09cac6fa60b47968ffddee96b
Filippo <filippo.pisello@live.it>
2022-01-17 17:29:39 +0100

    docs: update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = "2021, Filippo Pisello"
 author = "Filippo Pisello"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.2"
+release = "0.1.2"
 
 
 # -- General configuration ---------------------------------------------------
