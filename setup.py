ff24efaf40d5254f948cf3b070f2ef9f6f0b8294
Filippo <filippo.pisello@live.it>
2022-02-18 11:41:45 +0100

    build: increase version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file:
 
 setup(
     name="easypred",
-    version="0.1.2",
+    version="0.1.3",
     description="Easily store, assess and compare predictions obtained through Machine Learning models.",
     long_description=long_description,
     long_description_content_type="text/markdown",
